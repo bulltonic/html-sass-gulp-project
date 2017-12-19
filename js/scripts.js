5efc37595531f7118dec216fca0b41db6ad7c5b3
bulltonic <mstrauss@tonicdesign.com>
2017-12-19 10:10:26 -0500

    Ran initial gulp build of project

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	// ==============================================================*/
+	// Remove loading class to "note" document is ready
+	// ==============================================================*/
+
+	$('body').removeClass('loading');
+
+});
+//# sourceMappingURL=maps/scripts.js.map
