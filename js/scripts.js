ca48f3e5ca4edbcbb91e1228e567e3aad6247a04
bulltonic <mstrauss@tonicdesign.com>
2017-12-19 10:45:46 -0500

    Added potential svg ajax call for server use and removed the tonic link from the template

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,17 @@ $(document).ready(function(){
 
 	$('body').removeClass('loading');
 
+	// ==============================================================*/
+	// Create node and insert SVG file after the body
+	// ==============================================================*/
+
+	// Remove local file and uncomment below once on the dev server
+
+	// $.get("./img/spritemap.svg", function(data) {
+	//   var div = document.createElement("div");
+	//   div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
+	//   document.body.insertBefore(div, document.body.childNodes[0]);
+	// });
+
 });
 //# sourceMappingURL=maps/scripts.js.map
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,4 +6,16 @@ $(document).ready(function(){
 
 	$('body').removeClass('loading');
 
+	// ==============================================================*/
+	// Create node and insert SVG file after the body
+	// ==============================================================*/
+
+	// Remove local file and uncomment below once on the dev server
+
+	// $.get("./img/spritemap.svg", function(data) {
+	//   var div = document.createElement("div");
+	//   div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
+	//   document.body.insertBefore(div, document.body.childNodes[0]);
+	// });
+
 });
